3f4d33893c0a8d28649d5a85f6d8d4e6e4bcd279
Ahsan ullah <ahsanullah0216@gmail.com>
2025-01-13 21:34:05 +0600

    implement login and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::inertia('/', 'Home')->name('home');
-Route::get('/register', [RegisterController::class, 'create'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
+
+
+
+
+
+
+
+
+
+require __DIR__ . '/auth.php';
