98c24eee757619edf898729976b8518af9403165
Ahsan ullah <ahsanullah0216@gmail.com>
2025-01-19 21:22:07 +0600

    updte profile information

--- resources/js/pages/Profile/Edit.vue
+++ resources/js/pages/Profile/Edit.vue
@@ -1,8 +1,13 @@
 <script setup>
+import UpdateInfo from "./Sections/UpdateInfo.vue";
 
-
+defineProps({
+    user: Object,
+    status: String,
+});
 </script>
-<template>
-    <div>Profile</div>
 
+<template>
+    <Head title="- Profile" />
+    <UpdateInfo :user="user" :status="status" />
 </template>
