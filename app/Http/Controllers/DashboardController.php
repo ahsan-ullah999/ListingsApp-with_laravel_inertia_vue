1effd951a7c58d65368d698e470d59aa6ea66ad5
Ahsan ullah <ahsanullah0215@gmail.com>
2025-02-10 14:46:25 +0600

    create user dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,16 @@ use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return Inertia::render('Dashboard');
+    public function index(Request $request)
+    {
+        $listings =
+            $request->user()->role !== 'suspended' ?
+            $request->user()->listings()->latest()->paginate(10) :
+            null;
+
+        return Inertia::render('Dashboard', [
+            'listings' => $listings,
+            'status' => session('status')
+        ]);
     }
 }
